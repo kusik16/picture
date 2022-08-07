20e02ee1290bf6be5c3214ffb6f0d384ef66e98a
kusik16 <slobodyanuck@gmail.com>
2022-08-07 10:00:21 +0300

    commit

---
+++ src/js/modules/accordion.js
@@ -0,0 +1,21 @@
+const accordion = (triggersSelector, itemsSelector) => {
+    const buttons = document.querySelectorAll(triggersSelector),
+        blocks = document.querySelectorAll(itemsSelector);
+
+    blocks.forEach((block) => {
+        block.classList.add("animated", "fadeInDown");
+    });
+
+    buttons.forEach((btn) => {
+        btn.addEventListener("click", function () {
+            if (!this.classList.contains("active")) {
+                buttons.forEach((btn) => {
+                    btn.classList.remove("active", "active-style");
+                });
+                this.classList.add("active", "active-style");
+            }
+        });
+    });
+};
+
+export default accordion;
